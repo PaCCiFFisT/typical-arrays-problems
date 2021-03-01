77bacadfc804d14101c3463328f7b01e077a9b57
PaCCiFFisT <pacciffist@gmail.com>
2021-03-02 00:24:45 +0200

    feat: complete solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (!array) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  } else {
+    return Math.min.apply(null, array);
+  };
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!array) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  } else {
+    return Math.max.apply(null, array);
+  };
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (!array) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  } else {
+    let avg = array.reduce((total, number) => total + number) / array.length;
+    return avg;
+  };
+};
